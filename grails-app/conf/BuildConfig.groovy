f9c9c72e0f671fec6a1a86560949ca0d1a39bb34
unknown <mamster@MININT-G6I2C7K.(none)>
2012-02-25 21:05:12 -0800

    Updated for AWS 1.3.3

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -4,32 +4,36 @@ grails.project.test.reports.dir = "target/test-reports"
 
 grails.project.dependency.resolution = {
 
-    inherits("global") {
+  inherits("global") {
+    exclude "commons-httpclient"
+  }
+
+  log "warn"
+
+  repositories {
+    mavenCentral()
+    grailsPlugins()
+    grailsHome()
+    grailsCentral()
+  }
+
+  dependencies {
+    compile 'net.java.dev.jets3t:jets3t:0.8.1'
+    compile 'javax.mail:mail:1.4.1'
+    compile 'org.apache.httpcomponents:httpcore:4.2-beta1'
+    compile 'org.apache.httpcomponents:httpclient:4.2-beta1'
+    runtime 'org.apache.httpcomponents:httpcore:4.2-beta1'
+    runtime 'org.apache.httpcomponents:httpclient:4.2-beta1'
+    compile 'commons-logging:commons-logging:1.1.1'
+    compile 'org.codehaus.jackson:jackson-core-asl:1.7.2'
+    compile 'com.amazonaws:aws-java-sdk:1.3.3', {
+      excludes 'stax-api', 'jackson-core-asl', 'commons-httpclient', 'commons-logging'
     }
+  }
 
-    log "warn" 
+  plugins {
 
-    repositories {	
-		mavenCentral()
-		grailsPlugins()
-        grailsHome()
-        grailsCentral()
-    }
+    build ':release:1.0.0.RC3'
 
-	dependencies {
-        compile 'net.java.dev.jets3t:jets3t:0.8.1'
-        compile 'javax.mail:mail:1.4.1'
-        compile 'commons-httpclient:commons-httpclient:3.1'
-        compile 'commons-logging:commons-logging:1.1.1'
-		compile 'org.codehaus.jackson:jackson-core-asl:1.7.2'
-		compile 'com.amazonaws:aws-java-sdk:1.2.12', {
-			excludes 'stax-api', 'jackson-core-asl', 'commons-httpclient', 'commons-logging'
-		}
-     }
-
-	plugins {
-		
-		build  ':release:1.0.0.RC3'
-        
-	}
+  }
 }
